400e759dbc666450aceea818c4e30f3ea65083b8
monpeco <monpeco@gmail.com>
2016-12-28 19:58:33 +0000

    Passing parameters from sample view to controller and directly to help view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,14 @@ class WelcomeController < ApplicationController
   def index
   end
   
+  def sample
+  end
+
   def help
     @message_controller = 'Message send from WelcomeController'
+    if (params.has_key?(:id))
+      @id_controller = params[:id] + '001'
+    end
   end 
   
 end
